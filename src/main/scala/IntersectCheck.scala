5184d5b611699b45992e368ffaa4bbcb6bc45320
fp-zhandongpan <qwer1234>
2016-09-19 09:26:24 +0800

    MinKNumbers

---
+++ src/main/scala/IntersectCheck.scala
@@ -0,0 +1,40 @@
+/**
+  * 微软亚院之编程判断俩个链表是否相交
+  * 给出俩个单向链表的头指针，比如h1，h2，判断这俩个链表是否相交。
+  * 为了简化问题，我们假设俩个链表均不带环。
+
+  * 问题扩展：
+  * 1. 如果链表可能有环列?
+  * 2. 如果需要求出俩个链表相交的第一个节点列?
+  */
+object IntersectCheck {
+  def main(args: Array[String]): Unit = {
+    val intersect = SLLNode(2, SLLNode(3, SLLNode(3)))
+
+    val h1 : SLLNode = SLLNode(1, intersect)
+    val h2 : SLLNode = SLLNode(5, SLLNode(6, intersect))
+
+    val t1 = findTail(h1)
+    val t2 = findTail(h2)
+    if (t1 == t2) {
+      println("1. intersected")
+    }
+
+
+  }
+
+  def findCircleStart(h : SLLNode) : SLLNode = {
+    //@todo need implements
+    null
+  }
+
+  def findTail(h : SLLNode): SLLNode = {
+    var node = h
+    while(node.next != null) {
+      node = node.next
+    }
+    node
+  }
+}
+
+case class SLLNode(value: Int, var next : SLLNode = null)
---
+++ src/main/scala/NumberPuzzle.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by panzd on 9/16/16.
+  */
+object NumberPuzzle {
+  def main(args: Array[String]): Unit = {
+    val a : Array[Int] = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val b : Array[Int] = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+
+    for(i <- 0 until 9) {
+      for (i <- 0 until 9) {
+        //@todo need implements
+      }
+    }
+  }
+}
